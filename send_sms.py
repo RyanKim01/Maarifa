c35759b5d3c620c5d789701a58bac42511c0484c
RyanKim01 <kyhekm@hotmail.com>
2015-10-24 13:47:03 -0700

    Simple change in respond messages with fixed flask module

--- send_sms.py
+++ send_sms.py
@@ -5,8 +5,7 @@ account_sid = "AC38b690c158eee466fea8748f957b8bc1"
 auth_token = "09c1eb561ee7c11767421f9893deca1b"
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(body="Jenny please?! I love you <3",
+message = client.messages.create(body="Where are Unathi's home girls?",
                                  to="+13102546839",
-                                 from_="+14804183140",
-                                 media_url="http://www.example.com/hearts.png")
+                                 from_="+14804183140")
 print(message.sid)
