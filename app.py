d2e8b98c87cd82fb5f7f1c720bb9553b5d71af7e
Unathi Chonco <choncou@gmail.com>
2015-10-25 01:33:29 -0700

    Workflow work on Math>Grade 1> Any Lesson

--- app.py
+++ app.py
@@ -1,37 +1,66 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from flask_restful import Resource, Api
+import json
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-class Topics(Resource):
+def request_all_lessons(subject, level):
+    r = requests.get('http://maarifa.herokuapp.com/api/lesson')
+    data = r.json()
+    lesson_list = []
+    for lessons in data['objects']:
+        if subject in lessons.values():
+            if lessons["lesson_level"] == level:
+                lesson_list.append(lessons)
+
+    return lesson_list
+
+def request_lesson(subject, level, title):
+    r = requests.get('http://maarifa.herokuapp.com/api/lesson')
+    data = r.json()
+    lesson_list = []
+    for lessons in data['objects']:
+        if subject in lessons.values():
+            if lessons["lesson_level"] == level and lessons["lesson_title"] == title:
+                return lessons["lesson_content"]
+
+    return lesson_list
+
+class Lessons(Resource):
 
     def post(self):
         return "TODO"
 
-    def get(self, subject, level):
+    def get(self, subject, level, title=None):
 
-        topname = subject+" at "+str(level)
+        if not title:
+            lesson_list = request_all_lessons(subject, level)
 
-        return Response((render_template("content.html", topicname=topname)), mimetype="text/html")
+            return Response((render_template("topics.html", subject=subject, level=level, topics=lesson_list)), mimetype="text/html")
+        else:
+            content = request_lesson(subject, level, title)
+            return Response((render_template("content.html", lessontitle=title, content=content)), mimetype="text/html")
 
-api.add_resource(Topics, '/sub/', '/sub/<string:subject>/<int:level>')
+
+api.add_resource(Lessons, '/<string:subject>/<int:level>', '/<string:subject>/<int:level>/<string:title>')
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/sub/math')
+@app.route('/Math')
 def math():
-    return render_template("levelchoice.html", subject="Math", sublink="sub/math")
+    return render_template("levelchoice.html", subject="Math")
 
-@app.route('/sub/eng')
+@app.route('/English')
 def eng():
-    return render_template("levelchoice.html", subject="English", sublink="sub/eng")
+    return render_template("levelchoice.html", subject="English")
 
-@app.route('/sub/sci')
+@app.route('/Science')
 def sci():
-    return render_template("levelchoice.html", subject="Science", sublink="sub/sci")
+    return render_template("levelchoice.html", subject="Science")
 
 if __name__ == '__main__':
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
